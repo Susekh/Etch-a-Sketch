c133078a664c820e3c8bbeeb5f9a3f678a67cc70
Subh <subhranshukhilar@outlook.com>
2023-06-18 23:47:24 +0530

    Made grids to change random color on hover in javascript

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 let cdiv = document.querySelector(".container");
 
+let gridNo = 16
+let amount = gridNo*gridNo;
 
-
-for (let i = 0; i < 256; i++){
-    const div = document.createElement('div');
-    div.style.cssText = "border: 1px solid black;background-color: beige; height: 25px; width: 25px";  
-    cdiv.appendChild(div);
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+for (let i = 0; i < amount; i++){
+    let grid = document.createElement('div');
+    grid.classList.add("grid-class");
+    grid.style.cssText = "border: 1px solid black;background-color: beige; height: 25px; width: 25px";  
+    grid.addEventListener('mouseover', () => {
+            grid.style.backgroundColor = getRandomColor();
+        });
+    cdiv.appendChild(grid);
 }
+
+
+
